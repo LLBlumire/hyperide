8420b895963eb87d975c7e4bb083049e16bfadae
Bruno Nascimento <bruno.hn@icloud.com>
2023-07-24 23:44:15 -0300

    docs: add sqlite to server-demo example

--- examples/server-demo/build.rs
+++ examples/server-demo/build.rs
@@ -1,6 +1,14 @@
 use std::path::Path;
 
+use dotenv_codegen::dotenv;
+
 fn main() {
+    dotenv::dotenv().ok();
+
+    let database_url = dotenv!("DATABASE_URL");
+
+    println!("DATABASE_URL: {}", database_url);
+
     hyperide::tailwind::bootstrap(
         Path::new("./tailwind.config.js"),
         Path::new("./src/tailwind.css"),
--- examples/server-demo/src/main.rs
+++ examples/server-demo/src/main.rs
@@ -1,14 +1,30 @@
+#[macro_use]
+extern crate dotenv_codegen;
+
+use axum::extract::State;
 use axum::{response::Html, routing::get, Router};
 use hyperide::{
     htmx::include_htmx, hyperide, hyperscript::include_hyperscript, tailwind::include_tailwind,
 };
+use sqlx::SqlitePool;
+use std::env;
 use std::net::SocketAddr;
 use tower_livereload::LiveReloadLayer;
 
 #[tokio::main]
 async fn main() {
+    let db = SqlitePool::connect(dotenv!("DATABASE_URL"))
+        .await
+        .expect("Failed to connect to SQLite");
+
+    sqlx::migrate!()
+        .run(&db)
+        .await
+        .expect("Failed to run migrations");
+
     let app = Router::new()
-        .route("/", get(todos))
+        .route("/", get(todos_homepage))
+        .with_state(db)
         .layer(LiveReloadLayer::new());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -18,78 +34,103 @@ async fn main() {
         .unwrap();
 }
 
-fn base_page(content: String) -> Html<String> {
-    Html(hyperide! {
+async fn todos_homepage(State(db): State<SqlitePool>) -> Html<String> {
+    let content = todos(db).await;
+
+    Html(layout(content))
+}
+
+fn layout(content: String) -> String {
+    hyperide! {
         <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <title>"Todo App"</title>
-            { include_tailwind!() }
-            { include_htmx!() }
-            { include_hyperscript!() }
-        </head>
-        <body class="min-h-screen bg-gray-200">
-
-            { content }
-        </body>
+          <html lang="en">
+          <head>
+              <title>"Todo App"</title>
+              { include_tailwind!() }
+              { include_htmx!() }
+              { include_hyperscript!() }
+          </head>
+          <body class="min-h-screen bg-gray-200">
+            {content}
+          </body>
         </html>
-    })
+    }
 }
 
-struct Todo {
+struct TodoProps {
+    id: i64,
     value: String,
-    completed: bool,
+    is_completed: bool,
 }
-impl Todo {
-    fn get(&self, _id: usize) -> String {
-        hyperide! {
-            <form class="flex gap-4 bg-gray-50 p-2 rounded">
-                <input type="checkbox" checked={self.completed} _="on change log 'hi'"/>
-                <div
-                    class="flex-grow cursor-pointer select-none"
-                    _="on click click() the previous <input/>"
-                >
-                    {self.value.as_ref()}
-                </div>
-                <input type="submit" value="Edit" class="text-gray-500" />
-                <input type="submit" value="Delete" class="text-gray-500" />
-            </form>
-        }
-    }
 
-    fn edit(&self, _id: usize) -> String {
-        hyperide! {
-            <form class="flex gap-4 bg-gray-50 p-2 rounded">
-                <input type="checkbox" />
-                <input type="text" value={self.value.as_str()} class="flex-grow" />
-                <input type="submit" value="Save" class="text-gray-500" />
-                <input type="submit" value="Delete" class="text-gray-500" />
-            </form>
-        }
+fn todo(
+    TodoProps {
+        id,
+        value,
+        is_completed,
+    }: TodoProps,
+) -> String {
+    hyperide! {
+        <form id={id} class="flex gap-4 bg-gray-50 p-2 rounded">
+            <input type="checkbox" checked={is_completed} _="on change log 'hi'"/>
+            <div
+                class="flex-grow cursor-pointer select-none"
+                _="on click click() the previous <input/>"
+            >
+                {value}
+            </div>
+            <input type="submit" value="Edit" class="text-gray-500" />
+            <input type="submit" value="Delete" class="text-gray-500" />
+        </form>
     }
 }
 
-async fn todos() -> Html<String> {
-    let todos: &[Todo] = &[
-        Todo {
-            value: "Make program work".into(),
-            completed: false,
-        },
-        Todo {
-            value: "Setup program with a really really long text entry here that just keeps on going and doesn't stop".into(),
-            completed: true,
-        },
-    ];
-    base_page(hyperide! {
-        <div class="my-4 p-4 max-w-screen-sm mx-auto">
-            <h1 class="text-xl font-bold mb-4">Todo App</h1>
-            <ul class="flex flex-col gap-2">{
-                todos
+async fn todos(db: SqlitePool) -> String {
+    let todos = sqlx::query!("SELECT * FROM todos").fetch_all(&db).await;
+
+    match todos {
+        Ok(todos) => {
+            hyperide! {
+              <div class="my-4 p-4 max-w-screen-sm mx-auto">
+                <h1 class="text-xl font-bold mb-4">Todo App</h1>
+
+                <form>
+                    <input
+                        type="text"
+                        name="todo"
+                        placeholder="What needs to be done?"
+                        class="w-full p-2 rounded"
+                    />
+
+                    <input
+                        type="submit"
+                        value="Add Todo"
+                        class="w-full mt-2 p-2 rounded bg-blue-500 text-white"
+                    />
+                </form>
+
+                <ul class="flex flex-col gap-2">{
+                  todos
                     .iter()
                     .enumerate()
-                    .map(|(id, todo)| todo.get(id))
+                    .map(move |(_, todo_record)| {
+                            todo(TodoProps {
+                                id: todo_record.id,
+                                value: todo_record.value.clone(),
+                                is_completed: todo_record.is_completed,
+                            })
+                        }
+                    )
                     .collect::<String>()
-            }</ul>
-        </div>
-    })
+                }</ul>
+              </div>
+            }
+        }
+        Err(_) => hyperide! {
+          <div class="my-4 p-4 max-w-screen-sm mx-auto">
+            <h1 class="text-xl font-bold mb-4">Todo App</h1>
+            <p class="text-red-500">There was an error fetching the todos</p>
+          </div>
+        },
+    }
 }
